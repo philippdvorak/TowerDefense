741ab34643e5177eddbd8e09605c56f34709daf2
DDDisch <philipp.dvorak@sz-ybbs.ac.at>
2019-02-27 15:59:47 +0100

    Inital Commit for Base Enemy

--- src/Enemy/BaseEnemy.java
+++ src/Enemy/BaseEnemy.java
@@ -1,5 +1,17 @@
 package Enemy;
 
 public class BaseEnemy {
+    private int speedX;
+    private int speedY;
+    private int lives;
 
+    public BaseEnemy(int speedX, int speedY) {
+        this.speedX = speedX;
+        this.speedY = speedY;
+        this.lives = 100;
+    }
+
+    public int getLives() {
+        return lives;
+    }
 }
