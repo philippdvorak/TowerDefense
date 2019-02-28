bb5b6417510ce650868fc95f0dec717ce425220f
DDDisch <philipp.dvorak@sz-ybbs.ac.at>
2019-02-28 07:31:42 +0100

    Added Shooting Boolean

--- src/Enemy/BaseEnemy.java
+++ src/Enemy/BaseEnemy.java
@@ -4,11 +4,20 @@ public class BaseEnemy {
     private int speedX;
     private int speedY;
     private int lives;
+    private boolean shooting;
 
     public BaseEnemy(int speedX, int speedY) {
         this.speedX = speedX;
         this.speedY = speedY;
         this.lives = 100;
+    }
+
+    public boolean isShooting() {
+        return shooting;
+    }
+
+    public void setShooting(boolean shooting) {
+        this.shooting = shooting;
     }
 
     public int getLives() {
