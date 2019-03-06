478caafeb7c67477c114ce43d97240033921fc44
DDDisch <philipp.dvorak@sz-ybbs.ac.at>
2019-03-06 14:46:18 +0100

    Changed Ballon Image to Online Image

--- src/Tower/FirstTower.java
+++ src/Tower/FirstTower.java
@@ -20,11 +20,7 @@ public class FirstTower extends ImageView
     public FirstTower(double x,double y)
     {
 
-        try {
-            this.setImage(new Image(new FileInputStream("/Users/michael/Downloads/sniper.png")));
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
+        this.setImage(new Image("https://banner2.kisspng.com/20180324/voe/kisspng-balloon-clip-art-blue-balloon-cliparts-5ab676069caae9.6687421615219072066417.jpg"));
         this.setVisible(true);
         this.setX(x);
         this.setY(y);
