3b8aab58ba899ac7d53b58cbc19882a928cd2f7e
DDDisch <philipp.dvorak@sz-ybbs.ac.at>
2019-03-08 07:31:30 +0100

    Changed the Dead Detection

--- src/Tower/MagierTower.java
+++ src/Tower/MagierTower.java
@@ -55,7 +55,7 @@ public class MagierTower extends ImageView
             for (BaseEnemy e : m) {
                 if (HitBox.intersects(e.getBoundsInLocal())) {
                     e.setLives(e.getLives() - 10);
-                    if(e.getLives() < 0) {
+                    if(e.getLives() <= 0) {
                         m.remove(e);
                     }
 
