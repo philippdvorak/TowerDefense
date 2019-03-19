a8e78598ab09771946110b773f7fec1674491fff
michi7801 <michael.kaltenbrunner2@sz-ybbs.ac.at>
2019-03-19 14:19:40 +0100

    Changed UI Elements

--- src/com/github/dddisch/towerdefense/main/Main.java
+++ src/com/github/dddisch/towerdefense/main/Main.java
@@ -19,6 +19,7 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
