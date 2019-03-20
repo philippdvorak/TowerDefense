117aadd521d8024a75d8fc10fa14dc3a7441263e
DDDisch <philipp.dvorka@sz-ybbs.ac.at>
2019-03-20 18:27:41 +0100

    Fixed Live Bug

--- src/com/github/dddisch/towerdefense/main/Main.java
+++ src/com/github/dddisch/towerdefense/main/Main.java
@@ -160,8 +160,8 @@ public class Main extends Application {
                     if (e.isDead()) {
                         mainLives.set(mainLives.get()-1);
                         System.out.println(mainLives);
-                        Platform.runLater(() ->{
-                                enemyVector.remove(e);
+                        enemyVector.remove(e);
+                        Platform.runLater(() -> {
                                 root.getChildren().remove(e);
                         });
                     }
